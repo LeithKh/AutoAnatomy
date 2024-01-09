843298f32683e662a6a4873f355a26b9ec8ef9a5
Leith Kh <laithkhouder@gmail.com>
2024-01-09 13:31:30 +0300

    Initial commit

---
+++ scripts/loading.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        var loadingScreen = document.getElementById('loading-screen');
+        loadingScreen.style.display = 'none';
+    }, 2000);
+});
